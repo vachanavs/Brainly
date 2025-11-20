1277fda859c2f2ea38563356ec7211bdd618e360
vachanavs <140875731+vachanavs@users.noreply.github.com>
2025-11-20 11:52:35 +0530

    Update Footer.tsx

--- frontend/src/components/homepageComponents/Footer.tsx
+++ frontend/src/components/homepageComponents/Footer.tsx
@@ -8,20 +8,20 @@ export function Footer() {
     return <div>
         <footer className="rounded-lg shadow-sm mt-40">
             <div className="w-full px-20 md:flex md:items-center md:justify-between">
-            <span className="flex justify-center items-center text-sm pb-4 text-gray-800 sm:text-center dark:text-gray-800">@2025 Made with <HeartIcon /> by Sada. All Rights Reserved.
+            <span className="flex justify-center items-center text-sm pb-4 text-gray-800 sm:text-center dark:text-gray-800">@2025 Made with <HeartIcon /> by Vachana. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm gap-5 font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="https://x.com/sada_7070?t=cv-feOIf_1NdjFMh-BNTPA&s=09" target="_blank" className="hover:underline me-4 md:me-6"><TwitterIcon/></a>
+                    <a href="https://x.com/__vachh__" target="_blank" className="hover:underline me-4 md:me-6"><TwitterIcon/></a>
                 </li>
                 <li>
-                    <a href="https://github.com/sada7070/" target="_blank" className="hover:underline me-4 md:me-6"><GitHub/></a>
+                    <a href="https://github.com/vachanavs/" target="_blank" className="hover:underline me-4 md:me-6"><GitHub/></a>
                 </li>
                 <li>
-                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=sadashivamurthysp10656@gmail.com" target="_blank" className="hover:underline me-4 md:me-6"><MainIcon/></a>
+                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=vachanavs70@gmail.com" target="_blank" className="hover:underline me-4 md:me-6"><MainIcon/></a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/sada70/" target="_blank" className="hover:underline me-4 md:me-6"><Linkedin/></a>
+                    <a href="https://www.linkedin.com/in/vachana-vs/" target="_blank" className="hover:underline me-4 md:me-6"><Linkedin/></a>
                 </li>
             </ul>
             </div>
